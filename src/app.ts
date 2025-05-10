363779f5f8b87f83769862c3b6e45c268ad12f0e
mrAxekash <aliaxandr_ku@mail.ru>
2025-05-10 23:15:58 +0300

    change server cors

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,3 @@
-// import express from 'express';
-// import http from 'http';
-// import socketio from "socket.io";
-//
-// // import { createServer } from 'node:http';
-// // import { fileURLToPath } from 'node:url';
-// // import { dirname, join } from 'node:path';
-// //import { Server } from 'socket.io';
-//
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketio(server);
-//
-// //const __dirname = dirname(fileURLToPath(import.meta.url));
-//
-// app.get('/', (req, res) => {
-//
-//     res.send('Hello ws server');
-//     //res.sendFile(join(__dirname, 'index.html'));
-// });
-//
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-// });
-//
-// server.listen(3009, () => {
-//     console.log('server *:3009');
-// });
-
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
@@ -36,16 +7,20 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server)({cors: {
-    methods: ['GET', 'POST'],
+const io = new Server(server, { cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET, POST'],
         credentials: true,
-        allowedHeaders: ['my-custom-header'],
+    }})
 
-    }});
+const cors = require("cors");
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 
 app.get('/', (req, res) => {
     res.send("Hello,it's ws server");
-    res.sendFile(join(__dirname, 'index.html'));
+
 });
 
 io.on('connection', (socket) => {
